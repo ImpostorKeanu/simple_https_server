dc7a22418fbea5199ba4fc2d9017464d25450131
Justin Angel <justin@arch4ngel.ninja>
2018-07-09 15:14:28 -0400

    adding README.md and updating server.py

---
+++ README.md
@@ -0,0 +1,53 @@
+# Purpose
+
+This python3.6 script simplifies the process of bringing a TLS encrypted HTTPS
+server online. It can generate a random self-signed certificate or accept values
+pointing to one on disk.
+
+# Examples
+
+## Getting Help
+
+```bash
+user@computer:simple_https_server~> python3.6 server.py -h
+usage: SimpleHTTPSServer [-h] --interface INTERFACE --port PORT
+                         [--certfile CERTFILE] [--keyfile KEYFILE]
+                         [--generate] [--gcertfile GCERTFILE] [--gkey GKEY]
+
+Start a listening HTTPS server.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --interface INTERFACE, -i INTERFACE
+                        Interface/IP address the server will bind to.
+  --port PORT, -p PORT  Port the server will listen on.
+  --certfile CERTFILE, -c CERTFILE
+                        Certificate file for the server to uce
+  --keyfile KEYFILE, -k KEYFILE
+                        Keyfile corresponding to certificate file
+  --generate, -g        Generate and use a self-signed certificate in /tmp.
+  --gcertfile GCERTFILE
+                        Path to certificate file to be generated.
+  --gkeyfile GKEYFILE   Path to keyfile to be generated.
+```
+
+## Generate Certificate and Start the Server
+
+```bash
+user@computer:simple_https_server~> python3 server.py -i 127.0.0.1 -p 8080 --generate
+
+SimpleHTTPSServer
+
+[+] Arguments validated successfully
+[+] Generating self signed certificate
+[+] Writing certificate and keyfile to disk
+[+] Running https server
+[+] CTRL^C to exit
+[+] Log records below
+
+127.0.0.1 - - [09/Jul/2018 15:08:05] "GET /server.py HTTP/1.1" 200 -
+^C
+[+] CTRL^C caught
+[+] Shutting down the server...
+[+] Exiting
+```
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3.6
 
+import argparse
 import http.server
+import ssl
 from OpenSSL import crypto, SSL
 from socket import gethostname
 from pprint import pprint
 from time import gmtime, mktime
-import ssl
-import argparse
 
 class ArgumentError(BaseException):
     pass
@@ -19,18 +19,28 @@ def sprint(s):
 
 def run_server(interface=None, port=None, keyfile=None, certfile=None):
 
-    sprint("Running https server")
-    sprint("CTRL^C to exit")
-    sprint("Log records below")
-    print()
     server_address = (interface, port)
-    httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+    httpd = http.server.HTTPServer(server_address,
+            http.server.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(httpd.socket,
         server_side=True,
         certfile=certfile,
         keyfile=keyfile,
         ssl_version=ssl.PROTOCOL_TLSv1)
-    httpd.serve_forever()
+
+    try:
+        sprint("Running https server")
+        sprint("CTRL^C to exit")
+        sprint("Log records below")
+        print()
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print()
+        sprint("CTRL^C caught")
+        sprint("Shutting down the server...")
+        httpd.shutdown()
+        sprint("Exiting")
+        print()
 
 def generate_certificate(certfile, keyfile):
 
@@ -42,12 +52,12 @@ def generate_certificate(certfile, keyfile):
 
     # create a self-signed cert
     cert = crypto.X509()
-    cert.get_subject().C = "US"
-    cert.get_subject().ST = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    cert.get_subject().L = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-    cert.get_subject().O = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
-    cert.get_subject().OU = "Some Organization"
-    cert.get_subject().CN = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
+    cert.get_subject().C  = "US"
+    cert.get_subject().ST = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    cert.get_subject().L  = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+    cert.get_subject().O  = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+    cert.get_subject().OU = "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD" 
+    cert.get_subject().CN = "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
     cert.set_serial_number(1000)
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(10*365*24*60*60)
@@ -55,19 +65,22 @@ def generate_certificate(certfile, keyfile):
     cert.set_pubkey(k)
     cert.sign(k, 'sha256')
 
+    sprint("Writing certificate and keyfile to disk")
+
     open(certfile, "wb").write(
         crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
     open(keyfile, "wb").write(
         crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
 
-    sprint("Done")
-
 if __name__ == '__main__':
 
+    # certificate defaults
+     # used for certificate generation
     certfile = '/tmp/self_signed.crt'
     keyfile = '/tmp/self_signed.key'
 
-    parser = argparse.ArgumentParser(description="Start a listening HTTPS server.")
+    parser = argparse.ArgumentParser(prog="SimpleHTTPSServer",
+        description="Start a listening HTTPS server.")
     parser.add_argument('--interface', '-i', required=True,
         help="Interface/IP address the server will bind to.")
     parser.add_argument('--port', '-p', required=True, type=int,
@@ -78,24 +91,39 @@ if __name__ == '__main__':
         help="Keyfile corresponding to certificate file")
     parser.add_argument('--generate', '-g', default=None, action='store_true',
         help="Generate and use a self-signed certificate in /tmp.")
+    parser.add_argument('--gcertfile', default=certfile,
+        help="Path to certificate file to be generated.")
+    parser.add_argument('--gkeyfile', default=keyfile,
+        help="Path to keyfile to be generated.")
     args = parser.parse_args()
 
+    # error message
+    m = None
+
+    # assure certificate arguments are as expected
     if not args.certfile and not args.keyfile and not args.generate:
         m = """Script requires either --generate to be set or both of arguments
         for the --certfile and --keyfile parameters."""
     elif args.certfile and args.keyfile and args.generate:
         m = """Script requires either arguments for the certfile and keyfile
         parameters or, alternatively, the generate argument; not both"""
-    else:
-        m = None
-
+    
     if m:
         raise ArgumentError(m)
+
+    print()
+    print(parser.prog)
+    print()
+    sprint("Arguments validated successfully")
 
     if args.generate:
         generate_certificate(certfile, keyfile)
         args.certfile = certfile
         args.keyfile = keyfile
 
-    args.__delattr__('generate')
+    # remove arguments prior to passing via **kwargs
+    for attr in ['generate', 'gcertfile', 'gkeyfile']:
+        args.__delattr__(attr)
+
+    # **args.__dict__ because lazy
     run_server(**args.__dict__)
